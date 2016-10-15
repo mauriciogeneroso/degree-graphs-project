e9f770120a70e1324de6128fcb8f11ab05475085
pigo <pigo@pigoUbuntu>
2016-10-15 01:46:54 -0300

    Reorganizado todo o código. Armazenado na próprio objeto do grafo todos os métodos que irá manipular o grafo. No classe da janela ficará apenas métodos a parte que não tem relação direta com o próprio grafo. A príncipio criei de forma incorreta os métodos que manipulam o grafo na própra classe da janela e por isso decidi fazer a reorganização, também para melhor reaproveitamento do código.
    Falta refazer o armazenamento, agora desta outra forma.

--- TrabalhoFaculdadeGrafos/src/Principal/Grafo.java
+++ TrabalhoFaculdadeGrafos/src/Principal/Grafo.java
@@ -1,58 +1,118 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Principal;
 
 /**
- *
- * @author pigo
+ *  Classe para armazenar a Matriz e lista de Adjacência e Incidência. O armazenamento direcionado ou não direcionado é feito de acordo
+ *  com a opção que o usuário seleciona na criação do grafo. Se selecionar direcionado a matriz será criada com caracteristicas de
+ *  direcionada, e assim por diante para todos os demais casos.
+ * 
+ * 
+ * Estudantes de Ciência da Computação - 4 fase.
+ * @author Luan Darabas
+ * @author Luiz Alexandre da Luz
+ * @author Maurício Generoso
+ * 
  */
 public class Grafo {
-
-    private int[][] matrizAdjNaoDir;
-    private int[][] matrizIncNaoDir;
-    private int[][] matrizAdjDir;
-    private int[][] matrizIncDir;
+    
+    // Matrizes
+    private int[][] matrizAdj;
+    private int[][] matrizInc;
+    
+    // Listas
+    private Lista[] listaAdj;
+    private Lista[] listaInc;
+    
+    // Nós e arestas
+    private String[] nos;
+    private String[] arestas;
     
     public Grafo(){
-        matrizAdjNaoDir = null;
-        matrizIncNaoDir = null;
-        matrizAdjDir = null;
-        matrizIncDir = null;
+        matrizAdj = null;
+        matrizInc = null;
+    
+        listaAdj = null;
+        listaInc = null;
+    
+        nos = null;
+        arestas = null;
+    }
+
+    public int[][] getMatrizAdj() {
+        return matrizAdj;
+    }
+
+    public int[][] getMatrizInc() {
+        return matrizInc;
+    }
+
+    public Lista[] getListaAdj() {
+        return listaAdj;
+    }
+
+    public Lista[] getListaInc() {
+        return listaInc;
+    }
+
+    public String[] getNos() {
+        return nos;
+    }
+
+    public void setNos(String[] nos) {
+        this.nos = nos;
+    }
+
+    public String[] getArestas() {
+        return arestas;
+    }
+
+    public void setArestas(String[] arestas) {
+        this.arestas = arestas;
     }
     
-    public int[][] getMatrizAdjNaoDir() {
-        return matrizAdjNaoDir;
+    public void limparMatriz(int[][] matriz){   
+        for (int i = 0; i < matriz.length; i++){
+            for (int j = 0; j < matriz[i].length; j++){
+                matriz[i][j] = 0;
+            }
+        }
     }
-
-    public void setMatrizAdjNaoDir(int[][] matrizAdjNaoDir) {
-        this.matrizAdjNaoDir = matrizAdjNaoDir;
+    
+    public void limparLista(Lista[] lista){
+        for (int i = 0; i < lista.length; i++){
+            lista[i] = null;
+        }
     }
-
-    public int[][] getMatrizIncNaoDir() {
-        return matrizIncNaoDir;
+    
+    public void imprimirMatriz(int[][] matriz){
+        for (int[] matriz1 : matriz) {
+            for (int j = 0; j < matriz1.length; j++) {
+                System.out.print(matriz1[j] + " ");
+            }
+            System.out.println("");
+        }
     }
-
-    public void setMatrizIncNaoDir(int[][] matrizIncNaoDir) {
-        this.matrizIncNaoDir = matrizIncNaoDir;
+    
+    public void imprimirLista(Lista[] lista){
+        for (Lista li : lista){
+            System.out.println(lista);
+        }
     }
-
-    public int[][] getMatrizAdjDir() {
-        return matrizAdjDir;
+    
+    public void capturarNos(String entradaNosUsuario, int qntNos){
+        String[] nosTemp = new String[qntNos];
+        
     }
-
-    public void setMatrizAdjDir(int[][] matrizAdjDir) {
-        this.matrizAdjDir = matrizAdjDir;
+    
+    public int quantidadeNos(){
+        return nos.length;
     }
-
-    public int[][] getMatrizIncDir() {
-        return matrizIncDir;
+    
+    public boolean noIsEmpty(){
+        return nos == null;
     }
-
-    public void setMatrizIncDir(int[][] matrizIncDir) {
-        this.matrizIncDir = matrizIncDir;
+    
+    public int quantidadeArestas(){
+        return arestas.length;
     }
-
+    
 }
---
+++ TrabalhoFaculdadeGrafos/src/Principal/Lista.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Principal;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author pigo
+ */
+public class Lista {
+    
+    private ArrayList<String> lista = new ArrayList<>();
+
+    public ArrayList<String> getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList<String> lista) {
+        this.lista = lista;
+    }
+    
+    public void addElemento(String valor){
+        lista.add(valor);
+    }
+
+    @Override
+    public String toString() {
+        return "Lista{" + "lista=" + lista + '}';
+    }
+}
