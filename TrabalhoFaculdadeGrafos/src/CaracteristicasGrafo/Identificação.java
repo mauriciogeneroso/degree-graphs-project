15974ea20346f3379c413d8bbd4e41b326b3bd22
jarvis <jarvis@JARVIS>
2016-10-17 16:43:07 -0200

    O método trim() retira os espaços em branco...

--- TrabalhoFaculdadeGrafos/src/CaracteristicasGrafo/Identificação.java
+++ TrabalhoFaculdadeGrafos/src/CaracteristicasGrafo/Identificação.java
@@ -8,29 +8,30 @@ package CaracteristicasGrafo;
  * @author Luiz Alexandre da Luz
  * @author Maurício Generoso
 */
+
 public class Identificação {
    
-public int VerifGrafoSimples(){
-    int v = 0;
-    return v;          
-}
-
-public int VerifGrafoPlanar(){
-int v = 0;
-
-return v;    
-}
+    public int VerifGrafoSimples() {
+        int v = 0;
+        
+        return v;          
+    }
     
-public int VerifGrafoCompleto(){
-int v = 0;
-
-return v;
+    public int VerifGrafoPlanar() {
+        int v = 0;
+        
+        return v;    
+    }
     
-}
-
-public int VerifGrafoConexo(){
-int v = 0;
-
-return v;
-}
+    public int VerifGrafoCompleto() {
+        int v = 0;
+        
+        return v;
+    }
+    
+    public int VerifGrafoConexo() {
+        int v = 0;
+        
+        return v;
+    }
 }
