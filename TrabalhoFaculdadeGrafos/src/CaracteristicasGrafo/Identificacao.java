f9c2f41ba5c8439ac94b9b35bad2b5fdc8a9d310
User <User@192.168.0.6>
2016-10-19 12:40:16 -0200

    Movido para Objetos

--- TrabalhoFaculdadeGrafos/src/CaracteristicasGrafo/Identificacao.java
+++ TrabalhoFaculdadeGrafos/src/CaracteristicasGrafo/Identificacao.java
@@ -46,8 +46,6 @@ public class Identificacao {
         Matriz mt = (MatrizAdj) grafo.getMatrizAdj();
         int nos = grafo.quantidadeNos();
         int aresta = grafo.quantidadeArestas();
-        
-
         return v;
     }
 
@@ -79,16 +77,18 @@ public class Identificacao {
     }
 
     @SuppressWarnings("empty-statement")
+
     public int VerifGrafoConexo(Grafo grafo) {
         int v = 0;
         int soma = 0;
+        int it = 1;
         Matriz mt = (MatrizAdj) grafo.getMatrizAdj();
 
         mt.imprimirMatriz();
 
         for (int i = 0; i < mt.getLinhas(); i++) {
             for (int j = 0; j < mt.getColunas(); j++) {
-                if (i != j && mt.getMatriz()[i][j] != 0) {
+                if (i == j || mt.getMatriz()[i][j] > 0) {
                     continue;
                 } else {
                     v = 1;
@@ -98,52 +98,64 @@ public class Identificacao {
         }
         if (v == 0) {
 
-            int n=0;
+            int n = 0;
+            int cont = 1;
             int nos = grafo.quantidadeNos();
-            int it = 1;
+
             int vetor[] = new int[grafo.quantidadeNos()];
             for (int a = 0; a < grafo.quantidadeNos(); a++) {
-                vetor[a] = 0;
+                vetor[a] = -1;
             }
             int vetorsomado = 0;
-
+            vetor[vetorsomado] = 0;
+            vetorsomado++;
             for (int i = 0; i < mt.getLinhas(); i++) {
                 for (int j = 0; j < mt.getColunas(); j++) {
-                    if (i != j && mt.getMatriz()[i][j] != 0) {
+
+                    if (j != i && mt.getMatriz()[i][j] > 0 && cont == 1) {
+                        cont = 20;
                         n = j;
+                        System.out.println("" + n + "alow");
+                        break;
                     }
-                    do {
-                        vetor[vetorsomado] = i;
-                        vetorsomado++;
-                        for ( i = n; i < mt.getLinhas(); i++) {
-                            for (j = 0; j < mt.getColunas(); j++) {
-                                if (i != j && mt.getMatriz()[i][j] != 0) {
-                                    for (int f = 0; f < grafo.quantidadeNos(); f++) {
-                                        if (vetor[f] != j) {
-                                            n = j;
-                                        } else {
-                                            v = 1;
-                                            break;
-                                        }
-
-                                    }
-                                }
-
-                            }
-                            it++;
-                        }
-
-                    } while (it != nos);
-
                 }
             }
-        }
-                if (v == 1) {
-                    System.out.println("O Grafo não é Conexo");
-                } else {
-                    System.out.println("O Grafo é Conexo");
+
+            vetor[vetorsomado] = n;
+            System.out.println("" + vetor[vetorsomado] + "");
+
+            do {
+                vetorsomado++;
+                cont = 1;
+
+                for (int i = n; i < mt.getLinhas(); i++) {
+                    for (int j = 0; j < mt.getColunas(); j++) {
+                        if (i != j && mt.getMatriz()[i][j] > 0) {
+                            for (int f = 0; f < grafo.quantidadeNos(); f++) {
+                                if (vetor[f] == j && cont == 1) {
+                                    cont = 20;
+                                }
+                            }
+                            if (cont == 1) {
+                                n = j;
+
+                            }
+                        }
+
+                    }
+
                 }
-                return v;
-}
-       
+                vetor[vetorsomado] = n;
+                it++;
+
+            } while (it <= nos);
+        }
+
+        if (v == 1) {
+            System.out.println("O Grafo não é Conexo");
+        } else {
+            System.out.println("O Grafo é Conexo");
+        }
+        return v;
+    }
 }
